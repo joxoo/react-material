9300578bafaaffb87e7bfb918f373cafd60150f7
karnoldt <kevin.arnoldt@holidaycheck.com>
2016-05-13 16:20:25 +0200

    add polyfill for picture

--- src/rich-media/picture.js
+++ src/rich-media/picture.js
@@ -1,9 +1,31 @@
 import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { getClasses, MaterialPropTypes } from  '../addons';
+
+const findMediaIndex = (sources, width) => {
+    const index = sources.reverse().findIndex((source) => {
+        const matches = source.media.match(/^\(([a-z\-]+): ([0-9]+)([a-z]+)\)$/);
+        if(!matches) {
+            return false;
+        }
+        const media = matches[1];
+        const unit = matches[3];
+        const sourceWidth = unit === 'rem' ? matches[2] * 16 : matches[2];
+        if (media === 'min-width' && sourceWidth > width) {
+            return true;
+        } else if (media === 'max-width' && sourceWidth < width) {
+            return true;
+        }
+        return false;
+    });
+
+    return index !== -1 ? (sources.length - 1) - index : -1;
+};
 
 @getClasses
 
-class Picture extends React.Component {
+
+class PictureElement extends React.Component {
 
     static propTypes = {
         sources : PropTypes.arrayOf(
@@ -18,15 +40,27 @@ class Picture extends React.Component {
         ).isRequired
     };
 
+    constructor(...args) {
+        super(...args);
+
+        this.state = { index: 0 };
+    }
+    componentDidMount() {
+        if (!Boolean(window.HTMLPictureElement)) {
+            const picture = ReactDOM.findDOMNode(this).getElementsByTagName('img')[0];
+            const sources = this.props.sources;
+            const width = picture.width;
+            const index = findMediaIndex(sources, width);
+            if (index !== -1) {
+                this.setState({ index });
+            }
+        }
+    }
+
     render() {
-        const {
-            sources,
-            ...others
-            } = this.props;
-        const fallback = {
-            src: sources[0].src,
-            alt: sources[0].alt
-        };
+        const { sources, ...others } = this.props;
+        const index = this.state.index;
+        const fallback = { src: sources[index].src, alt: sources[index].alt };
 
         return (
             <picture className={ this.getClasses('picture', others) }>
@@ -34,10 +68,10 @@ class Picture extends React.Component {
                     const { src, type, media, sizes } = props;
                     return <source key={ key } { ...{media, sizes, type} } srcSet={ src }/>;
                 }) }
-                { fallback && <img {...fallback} {...others} /> }
+                { fallback && <img  {...fallback} {...others} /> }
             </picture>
         );
     }
 }
 
-export default Picture;
+export default PictureElement;
