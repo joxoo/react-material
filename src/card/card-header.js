b3e0bb51e250353586143c746d8a6c5836227b9f
karnoldt <kevin.arnoldt@holidaycheck.com>
2016-05-24 21:11:25 +0200

    make cardheader stateless

--- src/card/card-header.js
+++ src/card/card-header.js
@@ -1,57 +1,46 @@
 import React, { PropTypes } from 'react';
 import Avatar from '../avatar';
-import { getClasses } from  '../addons';
+import { getClassesStatic } from  '../addons/get-classes';
 
-@getClasses
+const CardHeader = (props) => {
+    const {avatar, title, subtitle, level, children, ...others} = props;
+    const ComponentTag = `h${level}`;
+    const hasAvatar = Boolean(avatar);
 
-class CardHeader extends React.Component {
-
-    static propTypes = {
-        avatar: PropTypes.shape({
-            icon: PropTypes.shape({
-                color: PropTypes.string,
-                icon: PropTypes.string.isRequired
-            }),
-            image: PropTypes.string,
-            letter:PropTypes.shape({
-                color: PropTypes.string,
-                character: PropTypes.string.isRequired
-            })
-        }),
-        title: PropTypes.string.isRequired,
-        subtitle: PropTypes.string,
-        level: PropTypes.number
-    };
-
-    static defaultProps = {
-        level: 2
-    };
-
-    render() {
-        const {
-            avatar,
-            title,
-            subtitle,
-            level,
-            ...others
-        } = this.props;
-        const ComponentTag = `h${level}`;
-
-        return (
-            <div {...others} className={ this.getClasses('card-header', others) }>
-                { avatar && <Avatar { ...avatar } className='card-header-avatar' /> }
-                <div className='card-header-text'>
-                    <ComponentTag className={ this.getClasses('card-header-title', { avatar: Boolean(avatar)}) }>
-                        { title }
-                    </ComponentTag>
-                    { subtitle &&
-                        <span className={ this.getClasses('card-header-subtitle', { avatar: Boolean(avatar)}) }>
-                            { subtitle }
-                        </span> }
-                </div>
+    return (
+        <div {...others} className={ getClassesStatic('card-header', others) }>
+            { avatar && <Avatar { ...avatar } className='card-header-avatar'/> }
+            <div className='card-header-text'>
+                <ComponentTag className={ getClassesStatic('card-header-title', { avatar: hasAvatar}) }>
+                    { title }
+                </ComponentTag>
+                { subtitle && <span className={ getClassesStatic('card-header-subtitle', { avatar: hasAvatar}) }>
+                    { subtitle }</span> }
+                { children }
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+CardHeader.propTypes = {
+    avatar: PropTypes.shape({
+        icon: PropTypes.shape({
+            color: PropTypes.string,
+            icon: PropTypes.string.isRequired
+        }),
+        image: PropTypes.string,
+        letter:PropTypes.shape({
+            color: PropTypes.string,
+            character: PropTypes.string.isRequired
+        })
+    }),
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string,
+    level: PropTypes.number
+};
+
+CardHeader.defaultProps = {
+    level: 2
+};
 
 export default CardHeader;
